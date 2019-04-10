c32965c6a5538429bf337b206b48f05a67ff38a9
bn8jx <bn8jx@mathcs03.richmond.edu>
2019-04-10 10:02:36 -0400

    Little modifications to the header files

--- TrafficLight.h
+++ TrafficLight.h
@@ -1,8 +1,29 @@
 #ifndef _TRAFFICLIGHT_H__
-#define _TRAFFICLIGHTH__
+#define _TRAFFICLIGHT_H__
+
+using namespace std;
+
+enum class Color { green, yellow, red };
 
 class TrafficLight {
 
+    public:
+
+        TrafficLight();
+        TrafficLight(const TrafficLight& other);
+        ~TrafficLight();
+
+        Color getColor();
+        Color updateState();
+        int getTimeRemaining();
+
+    protected:
+
+        int gLength;
+        int yLength;
+        int rLength;
+        int remaining;        
+
 };
 
-#ifndef 
+#endif
