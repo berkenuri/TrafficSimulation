7df814f66ea753210d9bb9b61223d4ea77d3fb4b
ml4gv <ml4gv@mathcs01.richmond.edu>
2019-04-17 09:53:41 -0400

    Comments for reading input file

--- testSimulation.cpp
+++ testSimulation.cpp
@@ -17,11 +17,13 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
+	// Create a vector to store the doubles read in from the input file
     vector<double> var;
 
 	ifstream in; 
 	in.open(argv[1]);
 	
+    // If input file format is incorrect, let the user know 
     if(in.bad()) {
 
         cout << "Bad Input File Format" << endl;
@@ -30,12 +32,15 @@ int main(int argc, char *argv[]) {
             
         string line;
             
+        // While there is another line of input to read
         while (getline(in, line)) {
             string label;
             stringstream linestream(line);
+            // Read the label, which is the whitespace after the ':'
             getline(linestream, label, ' ');
             cout << label << endl;
             double value;
+            // Read the double at the end of the input line
             linestream >> value;
 
             cout << value << endl;   
