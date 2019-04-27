585e010cc48361cebba235c0239f059cb8805a7f
bn8jx <bn8jx@mathcs03.richmond.edu>
2019-04-27 13:23:23 -0400

    commented the vehicle class

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -3,10 +3,13 @@
 
 #include "Vehicle.h"
 
+// default constructor
 Vehicle::Vehicle() {}
 
+// deconstructor
 Vehicle::~Vehicle() {}
 
+// copy constructor
 Vehicle::Vehicle(const Vehicle& other) {
 
     name = other.name;
@@ -19,13 +22,15 @@ Vehicle::Vehicle(const Vehicle& other) {
     turnRight = other.turnRight;
 }
 
+// constructor
 Vehicle::Vehicle(VehicleType vehicleName, 
                 int frontX, int frontY, 
                 int backX, int backY, 
                 Direction moveDirection, bool rightTurn) {
 
     name = vehicleName;
-    
+
+    // sets the size of the vehicle depending on its type
     if (vehicleName == VehicleType::car) 
         size = 2;
     else if (vehicleName == VehicleType::suv)
@@ -43,21 +48,25 @@ Vehicle::Vehicle(VehicleType vehicleName,
     turnRight = rightTurn;
 }
 
+// returns the name of the vehicle
 VehicleType Vehicle::getName() {
 
     return name;
 }
 
+// returns the front x coordinate of the vehicle
 int Vehicle::getFrontXPos() {
 
     return frontXPos;
 }
 
+// returns the front y coordinate of the vehicle
 int Vehicle::getFrontYPos() {
 
     return frontYPos;
 }
 
+// returns the front x and y coordinates of the vehicle
 vector<int> Vehicle::getFrontPos() {
 
     vector<int> frontCoordinates;
@@ -68,16 +77,19 @@ vector<int> Vehicle::getFrontPos() {
     return frontCoordinates;
 }
 
+// returns the back x coordinate of the vehicle
 int Vehicle::getBackXPos() {
 
     return backXPos;
 }
 
+// returns the back y coordinates of the vehicle
 int Vehicle::getBackYPos() {
 
     return backYPos;
 }
 
+// gets the back x and y coordinates of the vehicle
 vector<int> Vehicle::getBackPos() {
 
     vector<int> backCoordinates;
@@ -89,28 +101,33 @@ vector<int> Vehicle::getBackPos() {
 
 }
 
+// sets the front x-y coordinates of the vehicle
 void Vehicle::setFrontPos(int x, int y) {
 
     frontXPos = x;
     frontYPos = y;
 }
    
+// sets the back x-y coordinates of the vehicle
 void Vehicle::setBackPos(int x, int y) {
 
     backXPos = x;
     backYPos = y;
 }
-     
+
+// returns the size of the vehicle
 int Vehicle::getSize() {
 
     return size;
 }
 
+// returns true if the vehicle is turning right, false otherwise
 bool Vehicle::turnsRight() {
 
     return turnRight;
 }
  
+// moves the vehicle one unit to the direction it is supposed to advance
 void Vehicle::go() {
     
     if (direction == Direction::north) {
--- Vehicle.h
+++ Vehicle.h
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+// listing of all vehicle types
 enum class VehicleType {
     car,
     suv,
@@ -12,6 +13,7 @@ enum class VehicleType {
     undefined
 };
 
+// listing of all possible directions vehicles can move
 enum class Direction {
     north,
     south,
@@ -23,38 +25,50 @@ class Vehicle {
        
     public:
 
+        // default constructor
         Vehicle();
         
+        // copy constructor
         Vehicle(const Vehicle& other);
        
+        // constructor
         Vehicle(VehicleType vehicleName, 
                 int frontX, int frontY, 
                 int backX, int backY, 
                 Direction moveDirection, bool rightTurn); 
         
+        // deconstructor
         ~Vehicle();
 
+        // gets the name of the vehicle
         VehicleType getName();
         
+        // gets the front x and y coordinates of the vehicle
         int getFrontXPos();
         int getFrontYPos();
         vector<int> getFrontPos();
         
+        // gets the back x and y coordiantes of the vehicle
         int getBackXPos();
         int getBackYPos(); 
         vector<int> getBackPos();
 
+        // sets the front or back x-y coordinates of the vehicle
         void setFrontPos(int x, int y);
         void setBackPos(int x, int y);
 
+        // returns the size of the vehicle
         int getSize();
 
+        // makes the vehicle move one unit to the direction it is supposed to advance
         void go();
 
+        // returns true if the vehicle is going to turn right in the intersection
         bool turnsRight();
 
     private:
 
+        // data fields of the vehicle
         VehicleType name;
         int size;
         int frontXPos;
